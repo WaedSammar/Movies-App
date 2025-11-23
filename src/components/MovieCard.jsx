3e96a8b8e4a2d154f2c2049ed9ac100ae6103e98
WaedSammar <waedsammar12@gmail.com>
2025-11-23 22:24:44 +0200

    create movies app

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,34 @@
+import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
+
+function MovieCard({ movie }) {
+
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext()
+  const favorite = isFavorite(movie.id)
+
+  function handleFavoriteClick(e) {
+    e.preventDefault()
+    if (favorite) removeFromFavorites(movie.id)
+    else addToFavorites(movie)
+  }
+
+  return <div className="movie-card">
+    <div className="movie-poster">
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+      <div className="movie-overlay">
+        <button
+          className={`favorite-btn ${favorite ? "active" : ""}`}
+          onClick={handleFavoriteClick}
+        >
+          ❤
+        </button>
+      </div>
+    </div>
+    <div className="movie-info">
+      <h3>{movie.title}</h3>
+      <p>{movie.release_date?.split("-")[0]}</p>
+    </div>
+  </div>
+}
+
+export default MovieCard
